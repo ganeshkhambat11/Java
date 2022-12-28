7de9b0257feb11d2c63b2fc3edd23bfac288961b
Ganesh Khambat <82468084+ganeshkhambat11@users.noreply.github.com>
2022-12-28 14:35:35 +0530

    Create ForLoop.java

---
+++ ForLoop.java
@@ -0,0 +1,8 @@
+//printing 1 to 10 numbers using for loop
+public class ForLoop{
+  public static void main(String args[]){
+    for(int i=1 ; i<=10 ; i++){
+      System.out.print(i+" ");
+    }
+  }
+}
